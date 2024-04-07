fbabb5b076909ba3b81c83f3918cd7caa41a8c64
shuttle <139487522+shuttlepro@users.noreply.github.com>
2024-04-08 01:14:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -157,7 +157,7 @@ const BLOG = {
   // 代码主题 @see https://github.com/PrismJS/prism-themes
   PRISM_THEME_PREFIX_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_PREFIX_PATH ||
-    'https://raw.githubusercontent.com/PrismJS/prism-themes/master/themes/prism-solarized-dark-atom.css', // 代码块默认主题
+    'https://npm.elemecdn.com/prism-themes/themes/prism-darcula.css', // 代码块默认主题
   PRISM_THEME_SWITCH: process.env.NEXT_PUBLIC_PRISM_THEME_SWITCH || true, // 是否开启浅色/深色模式代码主题切换； 开启后将显示以下两个主题
   PRISM_THEME_LIGHT_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_LIGHT_PATH ||
